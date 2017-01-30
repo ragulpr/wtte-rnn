12ee5243fff085fa49afcba45b712d668c75fc28
ragulpr <test@youremail.com>
2017-01-30 15:49:54 +0100

    moved weibull funs, weibull_mean=continous mean
    Clarified in coments

--- src/weibull_helper/weibull_functions.py
+++ src/weibull_helper/weibull_functions.py
@@ -20,22 +20,25 @@ def weibull_pmf(t, a, b):
     return weibull_cdf(t+1.0, a, b)-weibull_cdf(t, a, b)
 
 def weibull_mode(a, b):
-    mode = a*np.power((b-1)/b,1/b)
-    mode[b<=1]=0
+    # Continuous mode. 
+    # Prooving how close it is to discretized mode TODO
+    mode = a*np.power((b-1.0)/b,1.0/b)
+    mode[b<=1.0]=0.0
     return mode
 
 def weibull_mean(a, b):
+    # Continuous mean. at most 1 step below discretized mean 
+    # E[T ] ≤ E[Td] + 1 true for positive distributions. 
     from scipy.special import gamma
     return a*gamma(1.0+1.0/b)
 
 def weibull_quantiles(a, b, p):
-    return a*np.power(-np.log(1-p),1/b)
-
+    return a*np.power(-np.log(1.0-p),1.0/b)
 
 def weibull_continuous_logLik(t, a, b, u=1):
     # With equality instead of proportionality. 
-    return u*np.log(weibull_pdf(t, a, b))+(1-u)*np.log(1-weibull_cdf(t, a, b))
+    return u*np.log(weibull_pdf(t, a, b))+(1-u)*np.log(1.0-weibull_cdf(t, a, b))
 
 def weibull_discrete_logLik(t, a, b, u=1):
     # With equality instead of proportionality. 
-    return u*np.log(weibull_pmf(t, a, b))+(1-u)*np.log(1-weibull_cdf(t+1, a, b))
+    return u*np.log(weibull_pmf(t, a, b))+(1-u)*np.log(1.0-weibull_cdf(t+1.0, a, b))
