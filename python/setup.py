8bcd27f7bff636091a9f81108235d7797dbc1c4b
Egil Martinsson <egil.martinsson@gmail.com>
2017-04-13 23:03:48 +0200

    cleanup,
    - is_discrete as param renamed discrete_time
    - added a test structure

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,10 @@ setup(name='wtte',
       description='Weibull Time To Event model. A Deep Learning model for churn- and failure prediction and everything else.',
       author='Egil Martinsson',
       author_email='egil.martinsson@gmail.com',
-      url='https://github.com/fchollet/wtte-rnn/',
+      url='https://github.com/ragulpr/wtte-rnn/',
       license='MIT',
       install_requires=['numpy', 'pandas', 'keras','tensorflow'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
       packages=find_packages())
+
