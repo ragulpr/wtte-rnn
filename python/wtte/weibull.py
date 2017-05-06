c16308147e5d69418fc5f46ddc5612fd4be778ff
Egil Martinsson <egil.martinsson@gmail.com>
2017-05-06 14:20:27 +0200

    Renaming weibull numpy functions
    - cleaner to call weibull.mean etc.
    - Added some TODO work on conditional excess distribution functions

--- python/wtte/weibull.py
+++ python/wtte/weibull.py
@@ -1,15 +1,19 @@
 import numpy as np
 ############################## Python weibull functions
-def cdf(t, a, b):
-    t = np.double(t)+1e-35
-    return 1-np.exp(-np.power(t/a,b))
+def cumulative_hazard(t, a, b):
+    t = np.double(t)
+    return np.power(t/a,b)
 
 def hazard(t, a, b):
-    t = np.double(t)+1e-35
+    t = np.double(t)
     return (b/a)*np.power(t/a,b-1)
 
+def cdf(t, a, b):
+    t = np.double(t)
+    return 1-np.exp(-np.power(t/a,b))
+
 def pdf(t, a, b):
-    t = np.double(t)+1e-35
+    t = np.double(t)
     return (b/a)*np.power(t/a,b-1)*np.exp(-np.power(t/a,b))
 
 def cmf(t, a, b):
@@ -42,46 +46,70 @@ def mean(a, b):
     from scipy.special import gamma
     return a * gamma(1.0 + 1.0 / b)
 
-def continuous_loglik(t, a, b, u=1):
-    # With equality instead of proportionality. 
-    return u*np.log(pdf(t, a, b))+(1-u)*np.log(1.0-cdf(t, a, b))
+def continuous_loglik(t, a, b, u=1, equality=False):
+    if equality:
+        # With equality instead of proportionality. 
+        loglik = u*np.log(pdf(t, a, b))+(1-u)*np.log(1.0-cdf(t, a, b))
+    else: 
+        # commonly optimized over: proportional terms w.r.t alpha,beta
+        loglik = u*loglik(hazard(t,a,b))-loglik(cumulative_hazard(t,a,b))
 
-def discrete_loglik(t, a, b, u=1):
-    # With equality instead of proportionality. 
-    return u*np.log(pmf(t, a, b))+(1-u)*np.log(1.0-cdf(t+1.0, a, b))
+    return loglik
 
-def cequantile(t, a, b, p):
-    # TODO this is not tested yet.
-    # tests: 
-    #    cequantile(0., a, b, p)==quantiles(a, b, p)
-    #    cequantile(t, a, 1., p)==cequantile(0., a, 1., p)
-    # conditional excess quantile
-    # t+s : Pr(Y<t+s|y>t)=p
-    
-    print 'not tested'
-    L = np.power((t+.0)/ a,b)
+def discrete_loglik(t, a, b, u=1,equality=False):
+    if equality:
+        # With equality instead of proportionality. 
+        loglik = u*np.log(pmf(t, a, b))+(1-u)*np.log(1.0-cdf(t+1.0, a, b))
+    else: 
+        # commonly optimized over: proportional terms w.r.t alpha,beta
+        hazard0 = cumulative_hazard(t,a,b)
+        hazard1 = cumulative_hazard(t+1.,a,b)
+        loglik = u*np.log(np.exp(hazard1-hazard0)-1.0)-hazard1
 
-    quantile = a * np.power(-np.log(1. - p) - L,1. / b)
+    return loglik
 
-    return quantile
+### Conditional excess
+class conditional_excess():
+    def pdf(t,s, a, b):
+        t = np.double(t)
+        return hazard(t+s,a,b)*np.exp(-cumulative_hazard(t+s)+cumulative_hazard(t))
 
-def cemean(t, a, b):
-    # TODO this is not tested yet.
-    # tests: 
-    #    cemean(0., a, b)==mean(a, b, p)
-    #    mean(t, a, 1., p)==mean(0., a, 1., p) == a
-    # conditional excess mean
-    # E[Y|y>t]
-    # (conditional mean age at failure)
-    # http://reliabilityanalyticstoolkit.appspot.com/conditional_distribution
-    from scipy.special import gamma
-    from scipy.special import gammainc
-    # Regularized lower gamma
-    print 'not tested'
+    def cdf(t,s, a, b):
+        t = np.double(t)
+        return 1-np.exp(-cumulative_hazard(t+s)+cumulative_hazard(t))
 
-    v = 1. + 1. / b
-    gv = gamma(v)
-    L = np.power((t+.0)/ a,b)
-    cemean = a * gv * np.exp(L) * (1 - gammainc(v, t / a) / gv)
+    def quantile(t, a, b, p):
+        # TODO this is not tested yet.
+        # tests: 
+        #    cequantile(0., a, b, p)==quantiles(a, b, p)
+        #    cequantile(t, a, 1., p)==cequantile(0., a, 1., p)
+        # conditional excess quantile
+        # t+s : Pr(Y<t+s|y>t)=p
+        
+        print 'not tested'
+        L = np.power((t+.0)/ a,b)
 
-    return cemean
+        quantile = a * np.power(-np.log(1. - p) - L,1. / b)
+
+        return quantile
+
+    def mean(t, a, b):
+        # TODO this is not tested yet.
+        # tests: 
+        #    cemean(0., a, b)==mean(a, b, p)
+        #    mean(t, a, 1., p)==mean(0., a, 1., p) == a
+        # conditional excess mean
+        # E[Y|y>t]
+        # (conditional mean age at failure)
+        # http://reliabilityanalyticstoolkit.appspot.com/conditional_distribution
+        from scipy.special import gamma
+        from scipy.special import gammainc
+        # Regularized lower gamma
+        print 'not tested'
+
+        v = 1. + 1. / b
+        gv = gamma(v)
+        L = np.power((t+.0)/ a,b)
+        cemean = a * gv * np.exp(L) * (1 - gammainc(v, t / a) / gv)
+
+        return cemean
