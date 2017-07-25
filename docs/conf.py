11e97eaa2f01d59893971b43b50d710127305ca0
Egil Martinsson <egil.martinsson@gmail.com>
2017-07-25 14:39:08 +0900

    refs #17 : conf.py from __future__ import print_function

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-
+from __future__ import print_function
 import sys
 import os
 
